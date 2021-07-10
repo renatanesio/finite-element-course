dfe74567253cbb014f148fb545c10e4bde8c1fb2
Renata Nésio <renata.nesio@mail.ru>
2021-07-10 08:45:19 -0300

    Implement quadrature

--- fe_utils/quadrature.py
+++ fe_utils/quadrature.py
@@ -44,7 +44,7 @@ class QuadratureRule(object):
         <ex-integrate>`.
         """
 
-        raise NotImplementedError
+        return np.dot([function(x) for x in self.points], self.weights)
 
 
 def gauss_quadrature(cell, degree):
